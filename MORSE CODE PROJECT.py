04705c74149f6c1a6a406eb633ef85eeb7588de9
Sadiq Fathia <jul846@usask.ca>
2024-12-31 20:16:49 -0600

    Initial commit

---
+++ MORSE CODE PROJECT.py
@@ -0,0 +1,33 @@
+#MORSE CODE PROJECT
+
+#morse_code = {"A":".-","B":"-...", "C":"-.-."}
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+text = input("Input string to be encrypted:\n")
+#print(text)
+
+def code_converter(string):
+    morse = ""
+    for i in string:
+        if i.upper() in MORSE_CODE_DICT:
+            morse += MORSE_CODE_DICT[i.upper()]
+        else:
+            print(i+" is not a character in morse code")
+    print (morse)
+        
+code_converter(text)
+    
